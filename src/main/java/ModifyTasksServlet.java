e4880470a0d968f93e0e881b4c742d1ae39917a4
DanielKaiGibbs <dan.kai.gibbs@gmail.com>
2021-03-07 12:07:41 +1300

    Update and add functionality seperated and implemented
    - The /update api now overrites existing entries with the specified title
    - The /add api now introduces a new task with the specified attributes
    - Error response when unsupported api is contacted

--- src/main/java/ModifyTasksServlet.java
+++ src/main/java/ModifyTasksServlet.java
@@ -15,6 +15,13 @@ public class ModifyTasksServlet extends HttpServlet {
 
     public void init() throws ServletException {}
 
+    /**
+     * Respond to API requests to fetch data from the database.
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
@@ -22,31 +29,51 @@ public class ModifyTasksServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        String JSONStringData = request.getParameter("taskData");
-        if (JSONStringData == null) {
-            out.println("ERROR: 'taskData' parameter must be provided");
+        //Extract the json describing the specified task
+        String taskData = request.getParameter("jsonData");
+        if (taskData == null) {
+            out.println("ERROR: 'jsonData' parameter must be provided");
             response.setStatus(400);
             return;
         }
 
         try {
-            JSONObject updateRequest = new JSONObject(JSONStringData);
+            JSONObject jsonRequest = new JSONObject(taskData);
+            String query = "";
 
-            //Check that the incoming JSON contains the correct attributes
-            String[] requiredAttributes = new String[]{"title", "description", "status", "due_date", "creation_date"};
-            if (!updateRequest.keySet().containsAll(Arrays.asList(requiredAttributes))) {
-                response.setStatus(400);
-                out.println("ERROR: Required attributes must be supplied in the json body: " + Arrays.toString(requiredAttributes));
-                return;
+            //Determine the query to apply to the database
+            if (request.getRequestURI().equals("/delete")) {
+
+            } else {
+                //Check that the incoming JSON contains the correct attributes to alter or add a new entry
+                String[] requiredAttributes = new String[]{"title", "description", "status", "due_date", "creation_date"};
+                if (!jsonRequest.keySet().containsAll(Arrays.asList(requiredAttributes))) {
+                    response.setStatus(400);
+                    out.println("ERROR: Required attributes must be supplied in the json body: " + Arrays.toString(requiredAttributes));
+                    return;
+                }
+                if (request.getRequestURI().equals("/add")) {
+                    query = "INSERT INTO tasks (title, description, status, due_date, creation_date) " +
+                            " VALUES (" +
+                            "'" + jsonRequest.get("title") + "', " +
+                            "'" + jsonRequest.get("description") + "', " +
+                            "'" + jsonRequest.get("status") + "', " +
+                            "'" + jsonRequest.get("due_date") + "', " +
+                            "'" + jsonRequest.get("creation_date") + "')";
+                }
+                else if (request.getRequestURI().equals("/update")) {
+                    query = "UPDATE tasks SET " +
+                            "description = '" + jsonRequest.get("description") + "'," +
+                            "status = '" + jsonRequest.get("status") + "'," +
+                            "due_date = '" + jsonRequest.get("due_date") + "'," +
+                            "creation_date = '" + jsonRequest.get("creation_date") + "'" +
+                            "WHERE title = '" + jsonRequest.get("title") + "'";
+                } else {
+                    response.setStatus(400);
+                    out.println("Error: Unsupported API request \"" + request.getRequestURI() + "\"");
+                    return;
+                }
             }
-
-            String query = "INSERT INTO tasks (title, description, status, due_date, creation_date) " +
-                    " VALUES (" +
-                    "'" + updateRequest.get("title") + "', " +
-                    "'" + updateRequest.get("description") + "', " +
-                    "'" + updateRequest.get("status") + "', " +
-                    "'" + updateRequest.get("due_date") + "', " +
-                    "'" + updateRequest.get("creation_date") + "')";
 
             System.out.println(query);
 
